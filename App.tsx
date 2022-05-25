b381f0ae641c14eda3da7ab964cea3a58ea6217b
gopinathraja <gopinath.rajamani1991@gmail.com>
2022-05-25 23:03:10 +0530

    Phase 1 devlopment

--- App.tsx
+++ App.tsx
@@ -1,11 +1,56 @@
 import * as React from 'react';
 import './style.css';
-
+var Lis = [
+  {
+    id: 1,
+    name: 'USA',
+    values: [
+      {
+        id: 2,
+        name: 'Chevy',
+        values: [
+          {
+            id: 3,
+            name: 'Suburban',
+          },
+          {
+            id: 4,
+            name: 'Camaro',
+          },
+        ],
+      },
+      {
+        id: 5,
+        name: 'Ford',
+      },
+    ],
+  },
+];
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <ListItem item={Lis[0]} />
     </div>
+  );
+}
+function ListItem({ item }) {
+  let children = null;
+  if (item.values) {
+    children = (
+      <ul>
+        {item.values.map((i) => (
+          <ListItem item={i} key={i.id} />
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <li>
+      {item.name}
+      {children}
+    </li>
   );
 }
